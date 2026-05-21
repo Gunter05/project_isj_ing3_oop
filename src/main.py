5ecd0a0ec309f8d0b8a9af6550acfca3e48331be
Anne-Onguene <anneonguene504@gmail.com>
2026-05-21 22:39:37 +0200

    initialisationdu main

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#importations
+from equipements import *  
+from topologie import * 
+from paquets import * 
+from securite import * 
+from moniteur import * 
+
+#Affichage du menu
+def afficher_menu():
+    print("\n===== SIMNET =====")
+    
+
+
+
