ab10f9ffa12fd355e4c6a2d8fcda893bc77749d7
Gunter <bitoukoaousmane@gmail.com>
2026-05-22 21:58:06 +0100

    feat: ajout de la validation IP, unicité des adresses et encapsulation de la CLI

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from paquets import Paquet
 from securite import Firewall as FirewallSecurite, RegleFiltrage, Protocole, Action
 from moniteur import MoniteurReseau
 
+from utils import valider_adresse_ip
+
 
 class FirewallPrincipal(FirewallSecurite):
     """
@@ -94,6 +96,15 @@ class SimulateurSIMNet:
 
         return Action.BLOQUER
 
+    def saisir_adresse_ip(self, message):
+        while True:
+            adresse_ip = input(message)
+
+            try:
+                return valider_adresse_ip(adresse_ip)
+            except ValueError as erreur:
+                print(erreur)
+
     # =========================
     # INITIALISATION
     # =========================
@@ -135,7 +146,7 @@ class SimulateurSIMNet:
         choix = input("Choix : ")
 
         nom = input("Nom : ")
-        adresse_ip = input("Adresse IP : ")
+        adresse_ip = self.saisir_adresse_ip("Adresse IP : ")
         marque = input("Marque : ")
 
         equipement = None
@@ -169,12 +180,13 @@ class SimulateurSIMNet:
             return
 
         equipement.activer()
-        self.topologie.ajouter_equipement(equipement)
+        if self.topologie.ajouter_equipement(equipement):
+            print(f"{nom} ajouté avec succès.")
+        else:
+            print("Ajout impossible.")
 
         if isinstance(equipement, FirewallPrincipal):
             self.firewall_principal = equipement
-
-        print(f"{nom} ajouté avec succès.")
 
     def afficher_equipements(self):
         if not self.topologie.equipements:
@@ -253,8 +265,8 @@ class SimulateurSIMNet:
             )
 
     def envoyer_paquet(self):
-        source = input("Adresse IP source : ")
-        destination = input("Adresse IP destination : ")
+        source = self.saisir_adresse_ip("Adresse IP source : ")
+        destination = self.saisir_adresse_ip("Adresse IP destination : ")
 
         print("\nService :")
         print("1. web")
